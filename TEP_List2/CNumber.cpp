ee9fc2a8b12f815cb4b54a385dfb019a63acf72e
BOTAGK <jakubbogacz05@gmail.com>
2025-10-20 18:23:01 +0200

    .

--- TEP_List2/CNumber.cpp
+++ TEP_List2/CNumber.cpp
@@ -1,1 +1,10 @@
 #include "CNumber.h"
+#include <iostream>
+
+CNumber::CNumber(){
+	i_length = number_default_length;
+	pi_number = new int[i_length];
+}
+CNumber::~CNumber() {
+	delete[] pi_number;
+}
--- TEP_List2/CNumber.h
+++ TEP_List2/CNumber.h
@@ -1,5 +1,27 @@
-#pragma once
+#ifndef CNumber_H
+#define CNUmber_H
+
 class CNumber
 {
+public:
+	static const int number_default_length = 10;
+
+	CNumber();
+	~CNumber();
+	void vSet(int iNewVal);
+	void vSet(CNumber& pcNewVal);
+	CNumber vAdd(CNumber& pcNewVal);
+	CNumber vSub(CNumber& pcNewVal);
+	CNumber vMul(CNumber& pcNewVal);
+	CNumber vDiv(CNumber& pcNewVal);
+	CNumber vAdd(int iNewVal);
+	CNumber vSub(int iNewVal);
+	CNumber vMul(int iNewVal);
+	CNumber vDiv(int iNewVal);
+private:
+	int* pi_number;
+	int i_length;
 };
+
+#endif
 
